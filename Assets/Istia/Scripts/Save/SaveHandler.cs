04ef800ace69b967398128bf12e1750b2d8d48f8
Doo <doo11gms@gmail.com>
2019-03-16 13:54:58 +0900

    [add]Save system

---
+++ Assets/Istia/Scripts/Save/SaveHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia.Save
+{
+    [CreateAssetMenu(menuName = "Save/SaveHandler")]
+    public class SaveHandler : SerializedScriptableObject
+    {
+        [OdinSerialize] List<ISavable> m_ISavables = new List<ISavable>();
+
+        [Button("Save")]
+	    public void Save()
+        {
+            if (m_ISavables == null) return;
+            m_ISavables.ForEach(savable => savable.Save());
+        }
+
+        [Button("Load")]
+        public void Load()
+        {
+            if (m_ISavables == null) return;
+            m_ISavables.ForEach(savable => savable.Load());
+        }
+    }
+}
