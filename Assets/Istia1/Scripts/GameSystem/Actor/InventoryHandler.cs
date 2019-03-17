2c4eb2361016c8c75f9c89103eef62d361d702e3
Doo <doo11gms@gmail.com>
2019-03-18 02:29:36 +0900

    [add]The function of pushing item to inventory

--- Assets/Istia1/Scripts/GameSystem/Actor/InventoryHandler.cs
+++ Assets/Istia1/Scripts/GameSystem/Actor/InventoryHandler.cs
@@ -24,15 +24,84 @@ namespace EllGames.Istia1.GameSystem.Actor
         [OdinSerialize] int QuestTabID { get; set; } = 4;
 
         /// <summary>
+        /// ItemInfoから適当なタブIDを取得します。
+        /// </summary>
+        /// <param name="itemInfo"></param>
+        /// <returns></returns>
+        int? GetTabID(DB.ItemInfo itemInfo)
+        {
+            if (itemInfo.Consumable) return ConsumableTabID;
+
+            return null;
+        }
+
+        /// <summary>
+        /// アイテムをプッシュ可能なスロットIDを返します。
+        /// 空のスロットよりもスタック可能なスロットが優先されます。
+        /// </summary>
+        /// <param name="itemInfo"></param>
+        /// <returns></returns>
+        int? PushableSlotID(DB.ItemInfo itemInfo)
+        {
+            var tabID = GetTabID(itemInfo);
+
+            if (tabID == null) return null;
+
+            int? emptySlotID = null;
+
+            foreach(var slotID in InventoryProfile.ChildSlotIDs((int)tabID))
+            {
+                if (InventoryProfile.IsEmpty((int)tabID, slotID))
+                {
+                    if (emptySlotID == null) emptySlotID = slotID;
+                    continue;
+                }
+
+                if (itemInfo.ItemID == InventoryProfile.GetItemID((int)tabID, slotID))
+                {
+                    if (InventoryProfile.GetItemCount((int)tabID, slotID) + 1 <= itemInfo.MaxStackCount) return slotID;
+                }
+            }
+
+            return emptySlotID;
+        }
+
+        /// <summary>
+        /// インベントリにアイテムを追加します。
+        /// </summary>
+        /// <param name="itemInfo"></param>
+        /// <returns></returns>
+        public bool PushItem(DB.ItemInfo itemInfo)
+        {
+            if (itemInfo == null) throw new System.Exception("nullなアイテムをプッシュすることはできません。");
+
+            var slotID = PushableSlotID(itemInfo);
+            if (slotID == null) return false;
+
+            var tabID = GetTabID(itemInfo);
+            if (tabID == null) return false;
+
+            var itemCount = 1;
+            if (!InventoryProfile.IsEmpty((int)tabID, (int)slotID)) itemCount = InventoryProfile.GetItemCount((int)tabID, (int)slotID) + 1;
+
+            return InventoryProfile.Assign((int)tabID, (int)slotID, itemInfo, itemCount);
+        }
+
+        /// <summary>
         /// インベントリにアイテムを追加します。
         /// </summary>
         /// <param name="itemInfo"></param>
         /// <param name="count"></param>
         /// <returns></returns>
+        [Button("Push Item")]
         public bool PushItem(DB.ItemInfo itemInfo, int count = 1)
         {
+            for (int i = 0; i < count; i++)
+            {
+                if (!PushItem(itemInfo)) return false;
+            }
 
-            return false;
+            return true;
         }
 
         /// <summary>
