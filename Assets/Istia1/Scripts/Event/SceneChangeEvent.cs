d78b24eb5ce0f5403c6a2c04250ea890a6d4bb62
Doo <doo11gms@gmail.com>
2019-03-18 17:51:53 +0900

    [add]title scene

---
+++ Assets/Istia1/Scripts/Event/SceneChangeEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia1.Event
+{
+    public class SceneChangeEvent : EventBase
+    {
+        [Title("Settings")]
+        [OdinSerialize] string NextSceneName { get; set; }
+
+        [Title("Fade Out")]
+        [OdinSerialize] bool UsingFadeOut { get; set; } = true;
+        [OdinSerialize, EnableIf("UsingFadeOut")] float Duration { get; set; } = 1f;
+
+        protected override void Execute()
+        {
+            if (NextSceneName == null) throw new System.Exception("遷移先のシーンが指定されていません。");
+
+            if (UsingFadeOut)
+            {
+                Gui.FadeManager.Instance.LoadScene(NextSceneName, Duration);
+            }
+            else
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(NextSceneName);
+            }
+        }
+    }
+}
