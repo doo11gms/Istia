2379ac41a7827e56b8e72f97ef47af7b3d32fbfb
Doo <doo11gms@gmail.com>
2019-03-18 18:00:20 +0900

    [add]Events; save, load, delete save data

---
+++ Assets/Istia1/Scripts/Event/AppQuitEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia1.Event
+{
+    public class AppQuitEvent : EventBase
+    {
+        protected override void Execute()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+    UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
