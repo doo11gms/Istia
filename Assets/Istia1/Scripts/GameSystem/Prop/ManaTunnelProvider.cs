300f0638bd93aa1ac75938629373f5608f9b0815
Doo <doo11gms@gmail.com>
2019-03-19 17:05:14 +0900

    [update]Mana tunnel

---
+++ Assets/Istia1/Scripts/GameSystem/Prop/ManaTunnelProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia1.GameSystem.Prop
+{
+    [CreateAssetMenu(menuName = "Istia1/Provider/ManaTunnelProvider", fileName = "ManaTunnelProvider")]
+    public class ManaTunnelProvider : SerializedScriptableObject
+    {
+        [OdinSerialize] List<ManaTunnel> ManaTunnels { get; set; } = new List<ManaTunnel>();
+        
+	    public ManaTunnel Provide(string id)
+        {
+            if (ManaTunnels == null) return null;
+
+            foreach(var found in ManaTunnels)
+            {
+                if (found.ID == id) return found;
+            }
+
+            return null;
+        }
+    }
+}
