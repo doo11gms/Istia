0cbec31571603d4668b5a1298b777a04728b69ac
Doo <doo11gms@gmail.com>
2019-03-21 19:19:47 +0900

    [update]teleport system

--- Assets/Istia1/Scripts/GameSystem/Actor/Behaviour/Move/MoveManager.cs
+++ Assets/Istia1/Scripts/GameSystem/Actor/Behaviour/Move/MoveManager.cs
@@ -12,6 +12,27 @@ namespace EllGames.Istia1.GameSystem.Actor.Behaviour.Move
 {
     public class MoveManager : SerializedMonoBehaviour
     {
+        [Title("Required")]
+        [OdinSerialize, Required] WayPointMove WayPointMove { get; set; }
+        [OdinSerialize, Required] TeleportMove TeleportMove { get; set; }
 
+        List<MoveBase> m_LatestAllowedMoves = new List<MoveBase>();
+
+
+
+        public void ReallowMove()
+        {
+
+        }
+
+        public void DisallowMove()
+        {
+        }
+
+        public void Teleport(Vector3? position = null, Vector3? eulerAngles = null)
+        {
+            TeleportMove.Teleport(position, eulerAngles);
+            WayPointMove.Stop();
+        }
     }
 }
---
+++ Assets/Istia1/Scripts/GameSystem/Map/MapLoadManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia1.GameSystem.Map
+{
+    public class MapLoadManager : SerializedMonoBehaviour
+    {
+        [Title("Required")]
+        [OdinSerialize, Required] Profile.PlayerProfile PlayerProfile { get; set; }
+
+        [Title("Settings")]
+        [OdinSerialize] string PlayerTag { get; set; } = "PlayerTag";
+
+        void MoveToNextScene()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(PlayerProfile.StartLocation.scene);
+        }
+
+        private void Start()
+        {
+            Actor.Behaviour.BehaviourController playerBehaviourController = null;
+
+            playerBehaviourController.Stop();
+            // teleport 
+
+            MoveToNextScene();
+        }
+    }
+}
