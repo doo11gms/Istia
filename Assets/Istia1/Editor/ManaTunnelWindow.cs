a827d9d2a8ca6e32c6d4fc99ae5bacf30df9fc1c
Doo <doo11gms@gmail.com>
2019-03-22 05:24:20 +0900

    [add]mana tunnel window for test

---
+++ Assets/Istia1/Editor/ManaTunnelWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace EllGames.Istia1.Editor
+{
+    public class ManaTunnelWindow : EditorWindow
+    {
+        // メニューからウィンドウを表示
+        [MenuItem("Window/Istia1/ManaTunnelWindow")]
+        public static void Open()
+        {
+            GetWindow<ManaTunnelWindow>(typeof(ManaTunnelWindow));
+        }
+
+        // ウィンドウを開いた時等に実行
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnGUI()
+        {
+            string manaTunnelName;
+            string sceneName;
+            Vector3 position;
+            Vector3 eulerAngles;
+        }
+    }
+}
