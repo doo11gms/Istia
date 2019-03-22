d005c98f18670752b9c179acd098c231f7738ddf
Doo <doo11gms@gmail.com>
2019-03-22 17:37:11 +0900

    [add]the namespace of Istia2 装備システムを綺麗に実装できるかテストするために名前空間を追加

---
+++ Assets/Istia2/Scripts/DB/EquipableItemInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia2.DB
+{
+    [CreateAssetMenu(fileName = "EquipableItemInfo", menuName = "Istia2/DB/EquipableItemInfo")]
+    public class EquipableItemInfo : ItemInfoBase
+    {
+
+    }
+}
