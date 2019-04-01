60eae0d271dee9f6e336b99b4bbf26ffd549a83c
Doo <doo11gms@gmail.com>
2019-04-01 20:59:00 +0900

    [add]equipment slot

---
+++ Assets/Istia4/Scripts/UI/Slot/EquipSlot.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia4.UI.Slot
+{
+    public class EquipSlot : SlotBase, Save.ISavable
+    {
+        void Save.ISavable.Save()
+        {
+            Save.SaveHandler.Save(this, m_EquipmentInfoID, nameof(m_EquipmentInfoID));
+        }
+
+        void Save.ISavable.Load()
+        {
+            Save.SaveHandler.Load(this, ref m_EquipmentInfoID, nameof(m_EquipmentInfoID));
+        }
+
+        [Title("Required")]
+        [OdinSerialize, Required] public DB.Inventory.EquipmentInfoProvider EquipmentInfoProvider { get; private set; }
+
+        [TitleGroup("Meta")]
+        [OdinSerialize] public DB.Inventory.EquipmentCategory EquipmentCategory { get; private set; }
+
+        [TitleGroup("Content")]
+        [OdinSerialize, ReadOnly] string m_EquipmentInfoID;
+        public string EquipmentInfoID
+        {
+            get { return m_EquipmentInfoID; }
+        }
+
+        [TitleGroup("Content")]
+        [OdinSerialize, ReadOnly] public DB.Inventory.EquipmentInfo EquipmentInfo { get; private set; }
+
+        [Title("UI Reference")]
+        [OdinSerialize] Image IconImage { get; set; }
+        [OdinSerialize] Image HoverOverlay { get; set; }
+        [OdinSerialize] Image PressOverlay { get; set; }
+        [OdinSerialize] Text EquipText { get; set; }
+
+        public void Emptimize()
+        {
+            m_EquipmentInfoID = null;
+            EquipmentInfo = null;
+            IconImage.sprite = null;
+            IconImage.gameObject.SetActive(false);
+            EquipText.gameObject.SetActive(false);
+        }
+
+        public bool Equip(DB.Inventory.EquipmentInfo equipmentInfo)
+        {
+            if (!IsEmpty())
+            {
+                Debug.Log("既に別の装備品を装備しているため、装備に失敗しました。");
+                return false;
+            }
+
+            if (equipmentInfo.EquipmentCategory != EquipmentCategory)
+            {
+                Debug.Log("カテゴリが異なるため、このスロットには装備できません。");
+                return false;
+            };
+
+            m_EquipmentInfoID = equipmentInfo.ID;
+            EquipmentInfo = equipmentInfo;
+            IconImage.sprite = equipmentInfo.IconSprite;
+            IconImage.gameObject.SetActive(true);
+            EquipText.gameObject.SetActive(true);
+
+            return false;
+        }
+
+        public DB.Inventory.EquipmentInfo Unequip()
+        {
+            if (IsEmpty()) throw new System.Exception("スロットが空であるため、装備解除に失敗しました。");
+
+            var buf = EquipmentInfo;
+            Emptimize();
+            return buf;
+        }
+
+        public bool IsEmpty()
+        {
+            return EquipmentInfo == null;
+        }
+
+        public void Refresh()
+        {
+            var info = EquipmentInfoProvider.Provide(m_EquipmentInfoID);
+            if (info == null) throw new System.Exception("EquipmentInfoの取得に失敗しました。");
+
+            Emptimize();
+            if (!Equip(info)) throw new System.Exception("取得したEquipmentInfoを装備できません。");
+        }
+    }
+}
