7313d29f090a3d40298a6bd5effd2b1b088791c9
Doo <doo11gms@gmail.com>
2019-04-02 03:38:46 +0900

    [add]inventory handler

---
+++ Assets/Istia4/Scripts/GameSystem/Actor/Player/InventoryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia4.GameSystem.Actor.Player
+{
+    public class InventoryHandler : SerializedMonoBehaviour
+    {
+        public bool Push()
+        {
+            return true;
+        }
+
+        public bool Dispose()
+        {
+            return true;
+        }
+
+        public bool DisposeAll()
+        {
+            return true;
+        }
+    }
+}
