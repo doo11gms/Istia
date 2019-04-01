4d7e5afb508d168181d729777d2f53dbb4f9f4f3
Doo <doo11gms@gmail.com>
2019-04-01 19:22:41 +0900

    [clean]

---
+++ Assets/Istia4/Scripts/UI/Window/EquipmentWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia4.UI.Window
+{
+    public class EquipmentWindow : WindowBase
+    {
+    }
+}
---
+++ Assets/Istia4/Scripts/UI/Window/WindowBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia4.UI.Window
+{
+    public abstract class WindowBase : SerializedMonoBehaviour
+    {
+        [Title("Settings")]
+        [OdinSerialize] bool CloseOnAwake { get; set; } = true;
+
+        [Button("Open")]
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+        [Button("Close")]
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void FlipOpen()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+
+        protected virtual void Awake()
+        {
+            if (CloseOnAwake) Close();
+        }
+    }
+}
