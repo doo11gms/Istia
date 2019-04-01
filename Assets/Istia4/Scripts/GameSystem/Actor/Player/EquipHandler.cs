581366dda141af0949e69ca7f7e038d2b3a259b2
Doo <doo11gms@gmail.com>
2019-04-01 21:50:22 +0900

    [add]XML document

--- Assets/Istia4/Scripts/GameSystem/Actor/Player/EquipHandler.cs
+++ Assets/Istia4/Scripts/GameSystem/Actor/Player/EquipHandler.cs
@@ -15,40 +15,63 @@ namespace EllGames.Istia4.GameSystem.Actor.Player
     {
         [OdinSerialize] UI.Window.EquipmentWindow EquipWindow { get; set; }
 
+        /// <summary>
+        /// 対象を装備します。
+        /// </summary>
+        /// <param name="slotID"></param>
+        /// <param name="equipmentInfo"></param>
+        /// <returns></returns>
         [Button("Equip")]
         public bool Equip(int slotID, DB.Inventory.EquipmentInfo equipmentInfo)
         {
             return EquipWindow.SearchSlot(slotID).Equip(equipmentInfo);
         }
 
+        /// <summary>
+        /// 装備解除します。
+        /// </summary>
+        /// <param name="slotID"></param>
+        /// <returns></returns>
         [Button("Unequip")]
         public bool Unequip(int slotID)
         {
             return EquipWindow.SearchSlot(slotID).Unequip();
         }
 
+        /// <summary>
+        /// すべての装備品を装備解除します。
+        /// </summary>
         [Button("Unequip All")]
         public void UnequipAll()
         {
             EquipWindow.EquipSlots.ForEach(slot => slot.Emptimize());
         }
 
-        [Button("Refresh")]
-        public void Refresh()
-        {
-            EquipWindow.EquipSlots.ForEach(slot => slot.Refresh());
-        }
-
+        /// <summary>
+        /// 装備品の情報をセーブします。
+        /// </summary>
         [Button("Save")]
         public void Save()
         {
             EquipWindow.EquipSlots.ForEach(slot => ((ISavable)slot).Save());
         }
 
+        /// <summary>
+        /// 装備品の情報をロードします。
+        /// </summary>
         [Button("Load")]
         public void Load()
         {
             EquipWindow.EquipSlots.ForEach(slot => ((ISavable)slot).Load());
+        }
+
+        /// <summary>
+        /// 装備品の情報をUIに反映します。
+        /// </summary>
+        [Button("Refresh")]
+        public void Refresh()
+        {
+            EquipWindow.EquipSlots.ForEach(slot => slot.Refresh());
         }
     }
 }
