5056f1736a647afbaccdbf43e9fb99d31dc93728
Doo <doo11gms@gmail.com>
2019-04-03 15:44:43 +0900

    [add]status system

---
+++ Assets/Istia4/Scripts/GameSystem/Actor/Player/PlayerStatus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia4.GameSystem.Actor.Player
+{
+    public class PlayerStatus : ActorStatusBase, Save.ISavable
+    {
+        void Save.ISavable.Save()
+        {
+        }
+
+        void Save.ISavable.Load()
+        {
+        }
+
+        /// <summary>
+        /// マップロード時に転送される場所です。
+        /// </summary>
+        [Title("Location")]
+        [OdinSerialize, InfoBox("Please set transferred location when map loaded.")] public Meta.Location StartLocation { get; set; }
+
+        /// <summary>
+        /// 死亡時に復活する場所です。
+        /// </summary>
+        [OdinSerialize, InfoBox("Please set respawn location when died.")] public Meta.Location RessurectionLocation { get; set; }
+    }
+}
