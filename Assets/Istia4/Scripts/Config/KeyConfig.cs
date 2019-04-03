0f876fd1e74f9af94f306eb5d61d1f0c854cc6c5
Doo <doo11gms@gmail.com>
2019-04-03 22:08:14 +0900

    [add]window manager

--- Assets/Istia4/Scripts/Config/KeyConfig.cs
+++ Assets/Istia4/Scripts/Config/KeyConfig.cs
@@ -12,8 +12,13 @@ namespace EllGames.Istia4.Config
 {
     public static class KeyConfig
     {
+        [Title("General")]
+        public static KeyCode CancelKey = KeyCode.Escape;
+
         [Title("Window")]
         public static KeyCode InventoryWindowKey = KeyCode.I;
+        public static KeyCode EquipmentWindowKey = KeyCode.E;
+        public static KeyCode HotKeyWindowKey = KeyCode.H;
 
         [Title("Inventory")]
         public static int UseItemMouseButton = 0;
---
+++ Assets/Istia4/Scripts/UI/Window/WindowManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Istia4.UI.Window
+{
+    public class WindowManager : SerializedMonoBehaviour
+    {
+        [Title("Required")]
+        [OdinSerialize, Required] InventoryWindow InventoryWindow { get; set; }
+        [OdinSerialize, Required] EquipmentWindow EquipmentWindow { get; set; }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(Config.KeyConfig.InventoryWindowKey))
+            {
+                InventoryWindow.FlipOpen();
+            }
+
+            if (Input.GetKeyDown(Config.KeyConfig.EquipmentWindowKey))
+            {
+                EquipmentWindow.FlipOpen();
+            }
+
+            if (Input.GetKeyDown(Config.KeyConfig.CancelKey))
+            {
+                InventoryWindow.Close();
+                EquipmentWindow.Close();
+            }
+        }
+    }
+}
