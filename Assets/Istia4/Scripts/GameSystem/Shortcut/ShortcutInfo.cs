8319caf849eab85064007bef29a9e6b6d42b7ee2
Doo <doo11gms@gmail.com>
2019-04-08 23:50:10 +0900

    [update]shortcut info

--- Assets/Istia4/Scripts/GameSystem/Shortcut/ShortcutInfo.cs
+++ Assets/Istia4/Scripts/GameSystem/Shortcut/ShortcutInfo.cs
@@ -12,6 +12,23 @@ namespace EllGames.Istia4.GameSystem.Shortcut
 {
     public class ShortcutInfo : SerializedMonoBehaviour
     {
-	    
+        [OdinSerialize] Sprite m_IconSprite;
+        public Sprite IconSprite
+        {
+            get { return m_IconSprite; }
+        }
+
+        [OdinSerialize] string m_TargetID;
+        public string TargetID
+        {
+            get { return m_TargetID; }
+        }
+
+        [OdinSerialize] SHORTCUT_TYPE m_ShortcutType;
+        public SHORTCUT_TYPE ShortcutType
+        {
+            get { return m_ShortcutType; }
+        }
+
     }
 }
