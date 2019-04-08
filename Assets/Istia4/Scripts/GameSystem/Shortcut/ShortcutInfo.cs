d1d31075725554e1f9aad012f9c6d05e4108ee2f
Doo <doo11gms@gmail.com>
2019-04-08 23:56:14 +0900

    [add]save system in shortcut info

--- Assets/Istia4/Scripts/GameSystem/Shortcut/ShortcutInfo.cs
+++ Assets/Istia4/Scripts/GameSystem/Shortcut/ShortcutInfo.cs
@@ -10,8 +10,22 @@ using Sirenix.Serialization;
 
 namespace EllGames.Istia4.GameSystem.Shortcut
 {
-    public class ShortcutInfo : SerializedMonoBehaviour
+    public class ShortcutInfo : SerializedMonoBehaviour, Save.ISavable
     {
+        void Save.ISavable.Save()
+        {
+            Save.SaveHandler.Save(this, m_TargetID, nameof(m_TargetID));
+            ES2.Save(m_IconSprite, GetInstanceID() + nameof(m_IconSprite));
+            ES2.Save(m_ShortcutType, GetInstanceID() + nameof(m_ShortcutType));
+        }
+
+        void Save.ISavable.Load()
+        {
+            Save.SaveHandler.Load(this, ref m_TargetID, nameof(m_TargetID));
+            m_IconSprite = ES2.Load<Sprite>(GetInstanceID() + nameof(m_IconSprite));
+            m_ShortcutType = ES2.Load<SHORTCUT_TYPE>(GetInstanceID() + nameof(m_ShortcutType));
+        }
+
         [OdinSerialize] Sprite m_IconSprite;
         public Sprite IconSprite
         {
